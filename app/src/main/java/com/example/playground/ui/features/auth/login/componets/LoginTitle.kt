147cc876b7e6bf68d563bbf1c66da7776d7bbf0d
Devkorrir <kipyegonaldo@proton.me>
2025-10-29 12:18:55 +0300

    tweak login

--- app/src/main/java/com/example/playground/ui/features/auth/login/componets/LoginTitle.kt
+++ app/src/main/java/com/example/playground/ui/features/auth/login/componets/LoginTitle.kt
@@ -1,9 +1,13 @@
 package com.example.playground.ui.features.auth.login.componets
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.dlete.ui.features.auth.signup.componets.AppColors
 
@@ -12,11 +16,20 @@ fun LoginTitle(
     modifier: Modifier = Modifier
 ) {
 
-    Text(
-        text = "Register",
-        fontSize = 32.sp,
-        fontWeight = FontWeight.Bold,
-        color = AppColors.Primary
-    )
+    Column {
+        Text(
+            text = "Welcome Back",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = AppColors.Primary
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = "Login to your account",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraLight,
+            color = AppColors.Primary
+        )
+    }
 
 }
